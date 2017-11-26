abc0915a5a76a62294e8d611aaf3caafe216c1a2
CloakedMC <31565677+CloakedMC@users.noreply.github.com>
2017-11-26 13:38:38 +0000

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+const client = new Discord.Cliend();
+
+cliend.on('ready', () => {
+	console.log('I am ready!');
+]);
+
+client.on('message, message => {
+	if (message.content === ping') {
+	  message.reply('pong')};
+	}
+});
+
+client.login(process.enc.nxTZAnLA1kV87v0cnaN6EL9ghumM23W3);
